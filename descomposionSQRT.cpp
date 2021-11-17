6f6b9b85a61f1d19935991146e8d8371623da1e1
HernandezJosze <65863648+HernandezJosze@users.noreply.github.com>
2021-11-16 22:10:44 -0600

    Update descomposionSQRT.cpp

--- descomposionSQRT.cpp
+++ descomposionSQRT.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <cmath>
-// HernandezJosze
+
 template<class T, typename F = T*(*)(T*, T*)>
 class sqrtTree{
     private: 
@@ -20,7 +20,7 @@ class sqrtTree{
             }
         }
 
-        T busca(const T& beg, const T& end){
+        T busca(const T& beg, const T& end){ // index begin, end 
             if(beg >= memory.size( )){
             return 0;
             }
@@ -38,6 +38,13 @@ class sqrtTree{
             }
             return *funcion(&result[0], &result[0] + result.size( ));
         }
+        void update(int index, T val){             // index to update, new value
+            std::vector<T> act = {memory[index], val};
+            memory[index] = val;
+            int ini = (index / segments) * segments;
+            int fin = ((index / segments) + 1) * segments;
+            leaders[index / segments] = *funcion(&memory[0] + ini, &memory[0] + fin);
+        }
 };
 
 int main( ){
@@ -51,6 +58,8 @@ int main( ){
     int* (*f)(int*, int*) = std::min_element;
     sqrtTree<int> sqrtT(v, f);
     
+    std::cout << sqrtT.busca(0, n - 1);
+    sqrtT.update(0, 0);
     std::cout << sqrtT.busca(0, n - 1);
 
     return 0;
